558289e8d129e882bb3564ebf38701cd2aded20f
Peter Shershov <stelaviv@gmail.com>
2018-11-04 03:06:21 +0200

    init

---
+++ src/ananas.js
@@ -0,0 +1,23 @@
+import { PureComponent } from "React";
+
+class Ananas extends PureComponent {
+	static demoProps = {
+		yeפ: "yep!",
+		working: {
+			piece: "of software",
+			recognizing: "objects",
+			like: {
+				a: ["boss"],
+				cause: "i am",
+				number: 1
+			}
+		}
+	}
+
+	render() {
+		return this.props.children
+
+	}
+}
+
+export default Ananas;
---
+++ src/demo-finder.js
@@ -0,0 +1,51 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Ananas from "./ananas";
+import { getType, isObject } from "mini-type";
+
+const demoProps = Ananas.demoProps;
+
+function getComponentByProp(prop) {
+	switch (getType(prop)) {
+		case "string":
+			return <span>"{prop}"</span>
+		case "number":
+			return <span>{prop}</span>
+		case "boolean":
+			return <span>{prop.toString()}</span>
+		case "object":
+			return componentsByObject(prop)
+		case "array":
+			return prop.map((element, index) => <div key={element + index}>
+				[
+				<div>{getComponentByProp(element)}</div>
+				]
+				</div>);
+	}
+}
+
+function componentsByObject(objectProp) {
+	const components = []
+	for (const key of Object.keys(objectProp)) {
+		if (isObject(objectProp[key])) {
+			componentsByObject(objectProp[key]);
+		}
+
+		components.push(<div key={key}>{key}: {getComponentByProp(objectProp[key])}</div>);
+	}
+
+	return components;
+}
+
+function generateStage(props) {
+	const components = [];
+	for (const key of Object.keys(demoProps)) {
+		components.push(<div key={key}>{key}: {getComponentByProp(demoProps[key])}</div>);
+	}
+
+	return components;
+}
+
+const Demo = props => <Ananas {...props}>{generateStage(demoProps)}</Ananas>
+
+ReactDOM.render(<Demo {...demoProps} />, document.querySelector(".root"));
